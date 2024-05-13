894bd606c314bb18b21caa639ec66f5647ee0829
chauhan-rahul@hotmail.com <chauhan-rahul@hotmail.com>
2024-05-13 21:41:24 +1000

    Command service added for the first time

---
+++ Controllers/PlatformServicesController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CommandService;
+
+[Route("api/c/[controller]")]
+[ApiController]
+public class PlatformServicesController : ControllerBase
+{
+    public PlatformServicesController()
+    {
+
+    }
+
+    [HttpPost]
+    public ActionResult TestInboundConnection()
+    {
+        Console.WriteLine("--> Inbound POST command.");
+        return Ok("--> Inbound connection is working just fine.");
+    }
+}
